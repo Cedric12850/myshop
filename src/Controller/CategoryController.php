3c6b744d0ff0d0763abc53b4160f60ce720c8cde
Cedric12850 <cedric.deswarte81@gmail.com>
2024-10-03 16:43:47 +0200

    commit initial

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Category;
+use App\Form\CategoryType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\DependencyInjection\DependencyInjectionExtension;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class CategoryController extends AbstractController
+{
+    #[Route('/category', name: 'app_category')]
+    public function index(): Response
+    {
+        return $this->render('category/index.html.twig', [
+            'controller_name' => 'CategoryController',
+        ]);
+    }
+
+    #[Route('/category/add', name: 'app_category_add')]
+    public function add(Request $request, EntityManagerInterface $entityManager ): Response
+    {
+        //Création d'un objet vide de type catégory
+        $newcategory = new Category;
+        //initialisation d'un formulaire à partir de la classe de formulaire correspondant à l'entité, puis on le relie à l'objet vide.
+        $form = $this->createForm(CategoryType::class, $newcategory );
+        //on demande au formulaire de traiter les requètes. On lui fournit donc un objet request injecté dans la fonction add()
+        $form->handleRequest($request);
+        //On lui indique quoi faire des données: surveiller si submit et valide
+        if($form->isSubmitted()&& $form->isValid()){
+            //alors on va remplir l'objet vide avec les données du formulaire
+            $newcategory = $form->getData();
+            // le persist va sauvegarder l'entité puis le flush va l'envoyer en bdd
+            $entityManager->persist($newcategory);
+            $entityManager->flush();
+
+        }
+        
+        return $this->render('category/add.html.twig', [
+            //on envoie le formulaire à la vue
+            'formulaire'=>$form
+        ]);
+    }
+}
---
+++ src/Controller/ProductController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use App\Form\ProductType;
+use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\BrowserKit\Response as BrowserKitResponse;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+class ProductController extends AbstractController
+{
+    #[Route('/product', name: 'app_product')]
+    public function index(ProductRepository $productRepository): Response
+    {
+        $products = $productRepository->findAll();
+        return $this->render('product/index.html.twig', [
+            'products' => $products
+        ]);
+    }
+
+    #[Route('/product/add', name: 'app_product_add')]
+    public function add(
+        Request $request,
+        EntityManagerInterface $entityManager,
+        SluggerInterface $slugger,
+        #[Autowire('%kernel.project_dir%/public/upload/')] string $uploadDirectory
+
+        ): Response
+    {
+        $newProduct = new Product;
+        $form = $this->createForm(ProductType::class, $newProduct);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted()&& $form->isValid()){
+             /** @var UploadedFile $brochureFile */
+             $thumbnail = $form->get('thumbnail')->getData();
+
+             if($thumbnail) {
+                //recupère le nom d'origine de l'image
+                $originalFilename = pathinfo($thumbnail->getClientOriginalName(), PATHINFO_FILENAME); 
+                //$slugger nettoie le nom de l'image en enlever les espaces ou autres, ...
+                $safeFilename = $slugger->slug($originalFilename);
+                //une fois le nom nettoyer, on lui attribue un id unique et guessExtension rajoute le .jpg ou . png ou autre
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$thumbnail->guessExtension();
+
+
+                // Move the file to the directory where brochures are stored
+                try {
+                    $thumbnail->move($uploadDirectory, $newFilename);
+                } catch (FileException $e) {
+                    // ... handle exception if something happens during file upload
+                }
+
+                // updates the 'uploadFilename' property to store the PDF file name
+                // instead of its contents
+                $newProduct->setThumbnail($newFilename);
+            }
+
+            $newProduct = $form->getData();
+            $entityManager->persist($newProduct);
+            $entityManager->flush();
+        }
+        return $this->render('product/add.html.twig', [
+            'formulaire'=>$form
+        ]);
+    }
+
+    //Afficher un article par son id
+    #[Route('/product/{id}', name: 'app_product_id')]
+    public function showById(ProductRepository $productRepository, int $id):Response
+    {
+        $product = $productRepository->find($id);
+        dump($product);
+        return $this->render('product/id.html.twig', [
+            'id' => $product
+        ]);
+    }
+
+
+    #[Route('/product/edit/{id}', name: 'app_product_edit')]
+    public function update(
+        EntityManagerInterface $entityManager,
+        int $id
+        ):Response
+        {
+            $product = $entityManager->getRepository(Product::class)->find($id);
+            if(!$product){
+                throw $this->createNotFoundException(
+                    "Le produit n'a pas été trouvé".$id
+                );
+            }
+            $product->setName();
+            $entityManager->flush();
+            return $this->redirectToRoute('app_product', [
+                'id'=>$product->getId()
+            ]);
+        }
+
+}
